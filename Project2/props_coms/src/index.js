7ee7088c1b6c24db1a2b62503970fa070389ec9f
Trystan <trystandames08@gmail.com>
2021-05-25 11:01:36 +0200

    16th Commit

--- Project2/props_coms/src/index.js
+++ Project2/props_coms/src/index.js
@@ -1,25 +1,18 @@
 import React, {Component} from 'react';
 import ReactDom from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail'
 
 class App extends Component {
     render() {
         return( 
-          <div className="ui container comments">
-              <div className="comment">
-                  <a href="/" className="avatar">
-                      <img alt="avatar" />
-                  </a>
-                  <div className="content">
-                      <a href="/" className="author">
-                          Sam
-                      </a>
-                      <div className="metadata">
-                          <span className="date">Today at 06:00pm</span>
-                      </div>
-                      <div className="text">Nice blog post!</div>
-                  </div>
-              </div>
-          </div> 
+            <div className="ui container comments">              
+               <CommentDetail />
+               <CommentDetail />
+               <CommentDetail />
+               <CommentDetail />
+               <CommentDetail />
+            </div> 
         );
     };
 }
