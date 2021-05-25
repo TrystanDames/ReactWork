a2e3db8568ec293f5d1d550dee23c5be3688854e
Trystan <trystandames08@gmail.com>
2021-05-25 12:05:50 +0200

    17th commit

--- Project2/props_coms/src/CommentDetail.js
+++ Project2/props_coms/src/CommentDetail.js
@@ -1,24 +1,28 @@
 import React, {Component} from 'react';
-import faker from 'faker';
 
 class CommentDetail extends Component {
+    
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return(
             <div className="comment">
                 <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()} />
+                    <img alt="avatar" src={this.props.avatar} />
                 </a>
                 <div className="content">
                     <a href="/" className="author">
-                        Sam
+                        {this.props.author}
                     </a>
                     <div className="metadata">
-                        <span className="date">Today at 06:00pm</span>
+                        <span className="date">{this.props.timeAgo}</span>
                     </div>
-                <div className="text">Nice blog post!</div>
+                <div className="text">{this.props.comment}</div>
                 </div>
             </div>
-        )
+        ) 
     };
 }
 
--- Project2/props_coms/src/index.js
+++ Project2/props_coms/src/index.js
@@ -2,16 +2,38 @@ import React, {Component} from 'react';
 import ReactDom from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail'
+import ApprovalCard from './ApprovalCard'
 
 class App extends Component {
     render() {
         return( 
-            <div className="ui container comments">              
-               <CommentDetail />
-               <CommentDetail />
-               <CommentDetail />
-               <CommentDetail />
-               <CommentDetail />
+            <div className="ui container comments">    
+                <ApprovalCard>
+                    <CommentDetail 
+                        author="Sam"
+                        timeAgo="Today at 16:45pm"
+                        comment="Nice Post!"
+                        avatar={faker.image.avatar()}
+                    />
+                </ApprovalCard>
+
+                <ApprovalCard>
+                    <CommentDetail
+                        author="Alex"
+                        timeAgo="Yesterday at 01:56am"
+                        comment="This is epic!!!"
+                        avatar={faker.image.avatar()}
+                    />
+                </ApprovalCard>
+                
+                <ApprovalCard>
+                    <CommentDetail 
+                        author="Jane"
+                        timeAgo="2 Days Ago at 10:27am"
+                        comment="You are a beast!"
+                        avatar={faker.image.avatar()}
+                    />
+                </ApprovalCard>
             </div> 
         );
     };
