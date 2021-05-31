8db11720bd902853c999fea1a9b46e7e26477c09
Trystan <trystandames08@gmail.com>
2021-05-31 12:32:14 +0200

    30th Commit

--- Project2/videos/src/components/VideoItem.js
+++ Project2/videos/src/components/VideoItem.js
@@ -1,18 +1,27 @@
 import React, {Component} from 'react';
 
-class VideoItem extends Component {
+// class VideoItem extends Component {
     
-    constructor(props){
-        super(props)
-    };
+//     constructor(props){
+//         super(props)
+//     };
 
-    render() {
-        return(
-            <div>
-                Video Item
-            </div>
-        )
-    };
+//     render() {
+//         return(
+//             <div>
+//                 Video Item
+//             </div>
+//         )
+//     };
+// }
+
+const VideoItem = ({video}) => {
+    return (
+        <div>
+            <img src={video.snippet.thumbnails.medium.url} />
+            {video.snippet.title} 
+        </div>
+    );
 }
 
 export default VideoItem;
