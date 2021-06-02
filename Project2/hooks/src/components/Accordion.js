040ace9440cfe885f6942051826a7c7596dec517
Trystan <trystandames08@gmail.com>
2021-06-02 11:38:42 +0200

    36th Commit, fixing error

--- Project2/hooks/src/components/Accordion.js
+++ Project2/hooks/src/components/Accordion.js
@@ -1,40 +1,62 @@
 import React, {Component} from 'react';
 
-class Accordion extends Component {
+// class Accordion extends Component {
 
-    constructor(props) {
-        super(props)
-    };
+//     constructor(props) {
+//         super(props)
+//     };
 
-    onTitleClick() {
-        console.log('Title clicked')
-    };
+//     onTitleClick() {
+//         console.log('Title clicked')
+//     };
 
-    render() {
+//     render() {
         
-        const renderedItems = this.props.items.map(({item, index}) => {
-            return(
-                <React.Fragment key={item.title}>
-                    <div
-                        className="title active"
-                        onClick={() => this.onTitleClick(index)}
-                    >
-                        <i className="dropdown icon"></i>
-                        {item.title}
-                    </div>
-                    <div className="content active">
-                        <p>{item.content}</p>
-                    </div>
-                </React.Fragment>
-            );
-        })
+//         const renderedItems = this.props.items.map(({item, index}) => {
+//             return(
+//                 <React.Fragment key={item.title}>
+//                     <div
+//                         className="title active"
+//                         onClick={() => this.onTitleClick(index)}
+//                     >
+//                         <i className="dropdown icon"></i>
+//                         {item.title}
+//                     </div>
+//                     <div className="content active">
+//                         <p>{item.content}</p>
+//                     </div>
+//                 </React.Fragment>
+//             );
+//         })
 
-        return(
-            <div className="ui styled accordion">
-                {renderedItems}
-            </div>
-        );
+//         return(
+//             <div className="ui styled accordion">
+//                 {renderedItems}
+//             </div>
+//         );
+//     };
+// }
+
+const Accordion = ({ items }) => {
+    const onTitleClick = (index) => {
+      console.log('Title clicked', index);
     };
-}
+  
+    const renderedItems = items.map((item, index) => {
+      return (
+        <React.Fragment key={item.title}>
+          <div className="title active" onClick={() => onTitleClick(index)}>
+            <i className="dropdown icon"></i>
+            {item.title}
+          </div>
+          <div className="content active">
+            <p>{item.content}</p>
+          </div>
+        </React.Fragment>
+      );
+    });
+  
+    return <div className="ui styled accordion">{renderedItems}</div>;
+  };
 
 export default Accordion;
