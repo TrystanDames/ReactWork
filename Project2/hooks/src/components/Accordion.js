a9a4ca2f702e7dba76451947d81862b1a175c912
Trystan <trystandames08@gmail.com>
2021-06-02 11:27:54 +0200

    35th commit

--- Project2/hooks/src/components/Accordion.js
+++ Project2/hooks/src/components/Accordion.js
@@ -5,12 +5,34 @@ class Accordion extends Component {
     constructor(props) {
         super(props)
     };
-    
+
+    onTitleClick() {
+        console.log('Title clicked')
+    };
+
     render() {
+        
+        const renderedItems = this.props.items.map(({item, index}) => {
+            return(
+                <React.Fragment key={item.title}>
+                    <div
+                        className="title active"
+                        onClick={() => this.onTitleClick(index)}
+                    >
+                        <i className="dropdown icon"></i>
+                        {item.title}
+                    </div>
+                    <div className="content active">
+                        <p>{item.content}</p>
+                    </div>
+                </React.Fragment>
+            );
+        })
+
         return(
-            <h1>
-                {this.props.items.length}
-            </h1>
+            <div className="ui styled accordion">
+                {renderedItems}
+            </div>
         );
     };
 }
