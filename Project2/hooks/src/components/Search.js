43da46d7fceb7bf4fee3995a3d89db8ec5e31c76
Trystan <trystandames08@gmail.com>
2021-06-04 10:16:20 +0200

    40th Commit

--- Project2/hooks/src/components/Search.js
+++ Project2/hooks/src/components/Search.js
@@ -1,29 +1,53 @@
-import React, {Component} from 'react';
+import React, {Component, useState, useEffect} from 'react';
 
-class Search extends Component {
+// class Search extends Component {
 
-    state = {term: ''}
+//     state = {term: ''}
 
-    onInputChange = event => {
-        this.setState({term: event.target.value})
-    };
+//     onInputChange = event => {
+//         this.setState({term: event.target.value})
+//     };
 
-    render() {
-        return(
-            <div>
-                <div className="ui form">
-                    <div className="field">
-                        <label>Enter Search Term</label>
-                        <input 
-                             value={this.state.term}
-                             onChange={this.onInputChange}
-                            className="input"    
-                        />
-                    </div>
+//     render() {
+//         return(
+//             <div>
+//                 <div className="ui form">
+//                     <div className="field">
+//                         <label>Enter Search Term</label>
+//                         <input 
+//                              value={this.state.term}
+//                              onChange={this.onInputChange}
+//                             className="input"    
+//                         />
+//                     </div>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
+
+const Search = () => {
+
+    const [term, setTerm] = useState('');
+
+    useEffect(() => {
+
+    }, [term])
+
+    return(
+        <div>
+            <div className="ui form">
+                <div className="field">
+                    <label>Enter Search Term</label>
+                    <input 
+                        value={term}
+                        onChange={(e) => setTerm(e.target.value)}
+                        className="input"    
+                    />
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default Search;
