b8bcaadfd66220d27ec76eae61d58fe538cfee21
Trystan <trystandames08@gmail.com>
2021-06-07 11:26:16 +0200

    45th Commit

--- Project2/hooks/src/components/Search.js
+++ Project2/hooks/src/components/Search.js
@@ -1,6 +1,8 @@
 import React, {Component, useState, useEffect} from 'react';
 import axios from 'axios';
 
+// My own work first
+
 // class Search extends Component {
 
 //     state = {term: ''}
@@ -27,9 +29,11 @@ import axios from 'axios';
 //     }
 // }
 
+// My work of video work
+
 const Search = () => {
 
-    const [term, setTerm] = useState('');
+    const [term, setTerm] = useState('programming');
     const [results, setResults] = useState([]);
 
     useEffect(() => {
@@ -40,35 +44,34 @@ const Search = () => {
                     list: 'search',
                     origin: '*',
                     format: 'json',
-                    srseach: term
-                }
+                    srsearch: term,
+                },
             });
 
             setResults(data.query.search);
         };
-        
-        if (term) {
-            search();
-        }
+
+       search();
     }, [term]);
 
     const renderedResults = results.map((result) => {
         return(
-            <div>
-                <div key={result.pageid} className="item">
-                    <div className="content">
-                        <div className="header">
-                            {result.title}
-                        </div>
-                        <span dangerouslySetInnerHTML={{__html: result.snippet}}></span>
-                    </div>
+            <div key={result.pageid} className="item">
+                <div className="right floated content">
+                    <a
+                        className="ui button"
+                        href={`https://en.wikipedia.org?curid=${result.pageid}`}
+                    >
+                        Go
+                    </a>
                 </div>
-                <div className="ui celled list">
-                    {renderedResults}
+                <div className="content">
+                    <div className="header">{result.title}</div>
+                    <span dangerouslySetInnerHTML={{__html: result.snippet}}></span>
                 </div>
             </div>
-        )
-    })
+        );
+    });
 
     return(
         <div>
@@ -82,8 +85,70 @@ const Search = () => {
                     />
                 </div>
             </div>
+            <div className="ui celled list">{renderedResults}</div>
         </div>
-    )
-}
+    );
+};
+
+
+//Video work below
+
+// const Search = () => {
+//     const [term, setTerm] = useState('programming');
+//     const [results, setResults] = useState([]);
+  
+//     useEffect(() => {
+//       const search = async () => {
+//         const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
+//           params: {
+//             action: 'query',
+//             list: 'search',
+//             origin: '*',
+//             format: 'json',
+//             srsearch: term,
+//           },
+//         });
+  
+//         setResults(data.query.search);
+//       };
+  
+//       search();
+//     }, [term]);
+  
+//     const renderedResults = results.map((result) => {
+//       return (
+//         <div key={result.pageid} className="item">
+//           <div className="right floated content">
+//             <a
+//               className="ui button"
+//               href={`https://en.wikipedia.org?curid=${result.pageid}`}
+//             >
+//               Go
+//             </a>
+//           </div>
+//           <div className="content">
+//             <div className="header">{result.title}</div>
+//             <span dangerouslySetInnerHTML={{__html: result.snippet}}></span>
+//           </div>
+//         </div>
+//       );
+//     });
+  
+//     return (
+//       <div>
+//         <div className="ui form">
+//           <div className="field">
+//             <label>Enter Search Term</label>
+//             <input
+//               value={term}
+//               onChange={(e) => setTerm(e.target.value)}
+//               className="input"
+//             />
+//           </div>
+//         </div>
+//         <div className="ui celled list">{renderedResults}</div>
+//       </div>
+//     );
+//   };
 
 export default Search;
