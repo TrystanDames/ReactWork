a4b6637003eafde96b6584d64e5b0a713a3ac3eb
Trystan <trystandames08@gmail.com>
2021-06-09 10:29:04 +0200

    52nd Commit

--- Project2/hooks/src/components/Dropdown.js
+++ Project2/hooks/src/components/Dropdown.js
@@ -1,12 +1,23 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
     const [open, setOpen] = useState(false);
+    const ref = useRef();
 
     useEffect(() => {
-        document.body.addEventListener('click', () => {
-            setOpen(false)
-        }, {capture: true}); // This is for React v17
+        const onBodyClick = (event) => {
+            if (ref.current && ref.current.contains(event.target)) {
+                return;
+            }
+
+            setOpen(false);
+        };
+
+        document.body.addEventListener('click', onBodyClick);
+
+        return () => {
+            document.body.removeEventListener('click', onBodyClick);
+        };
     }, []);
 
     const renderedOptions = options.map((option) => {
@@ -26,7 +37,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
     });
 
     return (
-        <div className="ui form">
+        <div ref={ref} className="ui form">
             <div className="field">
                 <label className="label">Select a Color</label>
                 <div
