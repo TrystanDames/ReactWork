d0baf8f04354e291432a6788116f7014305c945c
Trystan <trystandames08@gmail.com>
2021-07-01 10:46:05 +0200

    78th Commit

--- Project2/streams/client/src/components/App.js
+++ Project2/streams/client/src/components/App.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import { Router, Route } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
