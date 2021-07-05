c20efaecaaf4d065aca15b06f0323248f0e6b7e2
Trystan <trystandames08@gmail.com>
2021-07-05 11:09:03 +0200

    80th Commit

--- Project2/streams/client/src/components/streams/StreamCreate.js
+++ Project2/streams/client/src/components/streams/StreamCreate.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React , {Component} from 'react';
 import {connect} from 'react-redux';
 import {createStream} from '../../actions';
 import StreamForm from './StreamForm';
 
-class StreamCreate extends React.Component {
+class StreamCreate extends Component {
   onSubmit = formValues => {
     this.props.createStream(formValues);
   };
