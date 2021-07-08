0f7b690298586f4ea5e01be80509e2f4cba4724d
Trystan <trystandames08@gmail.com>
2021-07-08 10:00:27 +0200

    83rd Commit

---
+++ Project2/managing_app_state/src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+    return [
+        {title: 'Javascript: The Good Parts', pages: 101},
+        {title: 'Harry Potter', pages: 43},
+        {title: 'The Dark Tower', pages: 95},
+        {title: 'Spud', pages: 38},
+        {title: 'Eloquent Ruby', pages: 1}
+    ];
+}
