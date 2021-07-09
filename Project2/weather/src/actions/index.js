82eaee175d801adc2a6e1bbbb27e08292defe995
Trystan <trystandames08@gmail.com>
2021-07-09 13:17:56 +0200

    84th Commit

---
+++ Project2/weather/src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '70048d0ae0d717e4d08c1b0e83af8a1a';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
